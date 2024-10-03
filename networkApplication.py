5e9350f9606f9f8e2aa4b062be255437bf1f1fab
Shaan Jassal <shaanjassal101@gmail.com>
2024-10-03 18:39:29 +0100

    networkApplication working

---
+++ networkApplication.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+import argparse
+import socket
+import os
+import sys
+import struct
+import time
+import random
+import traceback # useful for exception handling
+import threading
+import select
+
+def setupArgumentParser() -> argparse.Namespace:
+        parser = argparse.ArgumentParser(
+            description='A collection of Network Applications developed for SCC.203.')
+        parser.set_defaults(func=ICMPPing, hostname='lancaster.ac.uk')
+        subparsers = parser.add_subparsers(help='sub-command help')
+        
+        parser_p = subparsers.add_parser('ping', aliases=['p'], help='run ping')
+        parser_p.set_defaults(timeout=4)
+        parser_p.add_argument('hostname', type=str, help='host to ping towards')
+        parser_p.add_argument('--count', '-c', nargs='?', type=int,
+                              help='number of times to ping the host before stopping')
+        parser_p.add_argument('--timeout', '-t', nargs='?',
+                              type=int,
+                              help='maximum timeout before considering request lost')
+        parser_p.set_defaults(func=ICMPPing)
+
+        parser_t = subparsers.add_parser('traceroute', aliases=['t'],
+                                         help='run traceroute')
+        parser_t.set_defaults(timeout=4, protocol='icmp')
+        parser_t.add_argument('hostname', type=str, help='host to traceroute towards')
+        parser_t.add_argument('--timeout', '-t', nargs='?', type=int,
+                              help='maximum timeout before considering request lost')
+        parser_t.add_argument('--protocol', '-p', nargs='?', type=str,
+                              help='protocol to send request with (UDP/ICMP)')
+        parser_t.set_defaults(func=Traceroute)
+        
+        parser_pt = subparsers.add_parser('paris-traceroute', aliases=['pt'],
+                                         help='run paris-traceroute')
+        parser_pt.set_defaults(timeout=4, protocol='icmp')
+        parser_pt.add_argument('hostname', type=str, help='host to traceroute towards')
+        parser_pt.add_argument('--timeout', '-t', nargs='?', type=int,
+                              help='maximum timeout before considering request lost')
+        parser_pt.add_argument('--protocol', '-p', nargs='?', type=str,
+                              help='protocol to send request with (UDP/ICMP)')
+        parser_pt.set_defaults(func=ParisTraceroute)
+
+        parser_w = subparsers.add_parser('web', aliases=['w'], help='run web server')
+        parser_w.set_defaults(port=8080)
+        parser_w.add_argument('--port', '-p', type=int, nargs='?',
+                              help='port number to start web server listening on')
+        parser_w.set_defaults(func=WebServer)
+
+        parser_x = subparsers.add_parser('proxy', aliases=['x'], help='run proxy')
+        parser_x.set_defaults(port=8000)
+        parser_x.add_argument('--port', '-p', type=int, nargs='?',
+                              help='port number to start web server listening on')
+        parser_x.set_defaults(func=Proxy)
+
+        args = parser.parse_args()
+        return args
+
+
+class NetworkApplication:
+
+    def checksum(self, dataToChecksum: str) -> str:
+        csum = 0
+        countTo = (len(dataToChecksum) // 2) * 2
+        count = 0
+
+        while count < countTo:
+            thisVal = dataToChecksum[count+1] * 256 + dataToChecksum[count]
+            csum = csum + thisVal
+            csum = csum & 0xffffffff
+            count = count + 2
+
+        if countTo < len(dataToChecksum):
+            csum = csum + dataToChecksum[len(dataToChecksum) - 1]
+            csum = csum & 0xffffffff
+
+        csum = (csum >> 16) + (csum & 0xffff)
+        csum = csum + (csum >> 16)
+        answer = ~csum
+        answer = answer & 0xffff
+        answer = answer >> 8 | (answer << 8 & 0xff00)
+
+        answer = socket.htons(answer)
+
+        return answer
+
+    def printOneResult(self, destinationAddress: str, packetLength: int, time: float, ttl: int, destinationHostname=''):
+        if destinationHostname:
+            print("%d bytes from %s (%s): ttl=%d time=%.2f ms" % (packetLength, destinationHostname, destinationAddress, ttl, time))
+        else:
+            print("%d bytes from %s: ttl=%d time=%.2f ms" % (packetLength, destinationAddress, ttl, time))
+
+    def printAdditionalDetails(self, packetLoss=0.0, minimumDelay=0.0, averageDelay=0.0, maximumDelay=0.0):
+        print("%.2f%% packet loss" % (packetLoss))
+        if minimumDelay > 0 and averageDelay > 0 and maximumDelay > 0:
+            print("rtt min/avg/max = %.2f/%.2f/%.2f ms" % (minimumDelay, averageDelay, maximumDelay))
+
+    def printMultipleResults(self, ttl: int, destinationAddress: str, measurements: list, destinationHostname=''):
+        latencies = ''
+        noResponse = True
+        for rtt in measurements:
+            if rtt is not None:
+                latencies += str(round(rtt, 3))
+                latencies += ' ms  '
+                noResponse = False
+            else:
+                latencies += '* ' 
+
+        if noResponse is False:
+            print("%d %s (%s) %s" % (ttl, destinationHostname, destinationAddress, latencies))
+        else:
+            print("%d %s" % (ttl, latencies))
